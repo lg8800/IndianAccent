bb159aba4854f872bd40a35c17020e23e18b951b
Lakshay Grover <lakshaygrover7@gmail.com>
2020-10-15 02:33:34 +0530

    Navigation Part 3

--- navigator/MainNavigator.js
+++ navigator/MainNavigator.js
@@ -1,8 +1,4 @@
 import { createAppContainer } from 'react-navigation';
-import Menu from '../components/MenuComponent';
-import Dishdetail from '../components/DishdetailComponent';
-import Home from '../components/HomeComponent';
-import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import HomeNavigator from './HomeNavigator';
 import MenuNavigator from './MenuNavigator';
@@ -10,10 +6,8 @@ import MenuNavigator from './MenuNavigator';
 const MainNavigator = createDrawerNavigator({
     Home: HomeNavigator,
     Menu: MenuNavigator
-}, {
-	contentOptions: {	
-    	activeBackgroundColor: 'D1C4E9'
-    }
+}, {	
+    	drawerBackgroundColor: '#D1C4E9'
 });
 
 export default createAppContainer(MainNavigator);
