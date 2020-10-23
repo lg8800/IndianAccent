6b70ec1159867b5db58fd8e0958f420dd8814770
Lakshay Grover <lakshaygrover7@gmail.com>
2020-10-23 19:31:44 +0530

    Using Redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import { View, Platform } from 'react-native';
 import MainNavigator from '../navigator/MainNavigator';
+import { connect } from 'react-redux';
+import { fetchComments, fetchDishes, fetchLeaders, fetchPromos} from '../redux/ActionCreators';
+
+const mapStateToProps = state => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+  fetchDishes: () =>dispatch(fetchDishes()),
+  fetchComments: () =>dispatch(fetchComments()),
+  fetchPromos: () =>dispatch(fetchPromos()),
+  fetchLeaders: () =>dispatch(fetchLeaders()),
+})
 
 class Main extends Component {
+
+  componentDidMount() {
+    this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchLeaders();
+    this.props.fetchPromos();
+  }
 
   render() {
  
@@ -14,4 +36,4 @@ class Main extends Component {
   }
 }
 
-export default Main;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
